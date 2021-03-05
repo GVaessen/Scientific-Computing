5aec8b466ec984df69d53203c8d768675b473805
guido <guidovaessen@hotmail.com>
2021-03-05 13:46:59 +0100

    added direct solver for diffusion
    also removed some plots and dead code

--- 2_1.py
+++ 2_1.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import copy
 from matplotlib import animation
 import pandas as pd
+from scipy.sparse import diags
 
 class Cell(object):
     '''
@@ -35,9 +36,10 @@ class Grid(object):
     seed: 2-tuple, coordinates of initial object
     steps: int, amount of times of new cell is attached to the object
     e: float, convergence meausure (when the difference between two SOR iterations is considered 0)
+    method: str, 'SOR' or 'shifts' determines which numerical method is used for the diffusion equation 
     '''
 
-    def __init__(self, shape, w, eta, seed, steps, epsilon):
+    def __init__(self, shape, w, eta, seed, steps, epsilon, method):
         self.Nrows = shape[0]
         self.Ncols = shape[1]
         self.seed = seed
@@ -45,6 +47,7 @@ class Grid(object):
         self.eta = eta
         self.steps = steps
         self.epsilon = epsilon
+        self.method = method
         self.cells = np.array([[Cell(0, False, False, np.random.random(), (i,j)) for j in np.arange(self.Ncols)] for i in np.arange(self.Nrows)])
         self.iterations = []
         self.body = [self.cells[seed[0], seed[1]]]
@@ -56,7 +59,7 @@ class Grid(object):
         self.cells[0, :] = np.array([Cell(1, False, False, np.random.random(), (0,j)) for j in np.arange(self.Ncols)])
         
         # initial body
-        self.cells[seed[0], seed[1]] = Cell(0, False, True, np.random.random(), (seed[0], seed[1]))
+        # self.cells[seed[0], seed[1]] = Cell(0, False, True, np.random.random(), (seed[0], seed[1]))
         
         # initial candidates
         self.cells[seed[0]-1, seed[1]] = Cell(0, True, False, np.random.random(), (seed[0]-1, seed[1]))
@@ -83,7 +86,31 @@ class Grid(object):
             for i in np.arange(self.Nrows):
                 self.cells[i, j].C = final_C[i]
 
-        self.cells[self.seed[0], self.seed[1]].C = 0
+        # self.cells[self.seed[0], self.seed[1]].C = 0
+        self.concentrations = self.cells_C()
+
+    def shift_solve(self):
+        '''
+        solve the diffusion equation directly instead of iteratively using matrix shifts, matrix must be diagonal
+        '''
+
+        old_C = np.ones((self.Nrows, self.Ncols))
+
+        while np.amax(np.abs(self.concentrations - old_C)) > self.epsilon:
+
+            old_C = self.concentrations.copy()
+
+            # construct L and U with 1's next to diagonals
+            L = diags(np.ones(self.Nrows-1), offsets=-1).toarray()
+            U = diags(np.ones(self.Nrows-1), offsets=1).toarray()
+ 
+            new_C = 0.25 * (L @ self.concentrations + self.concentrations @ L + U @ self.concentrations + self.concentrations @ U)
+            
+            # implement boundary conditions
+            new_C[0] = np.ones(self.Ncols)
+            new_C[-1] = np.zeros(self.Ncols)
+            
+            self.concentrations = new_C
 
         
     def SOR(self):
@@ -93,16 +120,10 @@ class Grid(object):
         n_iter = 0        
         old_C = np.ones((self.Nrows, self.Ncols))
 
-        # self.concentrations = self.cells_C()
-
-        # while np.amax(np.abs(self.cells_C() - old_C)) > self.epsilon:
         while np.amax(np.abs(self.concentrations - old_C)) > self.epsilon:
-            # print('yeet', np.amax(np.abs(self.concentrations - old_C)))
 
             n_iter += 1
 
-            # old_C = copy.deepcopy(self.cells_C())
-            # old_C = self.concentrations.copy()
             old_C = self.concentrations[:]
 
             full_copy = copy.deepcopy(self.cells)
@@ -114,9 +135,6 @@ class Grid(object):
 
                     if self.cells[i, j].in_body:
                         continue
-                    
-                    # # GS method
-                    # self.cells[i, j].C = 0.25 * (old_C[i+1, j] + self.cells[i-1, j].C + old_C[i, (j+1)%self.Ncols] + self.cells[i, j-1].C)
                     
                     neighbours = [self.cells[i-1, j], self.cells[i, j-1], full_copy[i+1, j], full_copy[i, (j+1)%self.Ncols]]
                     n_neighbours = len([neighbour for neighbour in neighbours if not neighbour.in_body])
@@ -135,7 +153,7 @@ class Grid(object):
             self.concentrations = new_C
 
         self.iterations += [n_iter]
-        # print(n_iter)
+
 
 
     def get_neighbours(self, i, j):
@@ -150,20 +168,11 @@ class Grid(object):
         '''
         returns a list of all candidates of in the grid
         '''
-        # for i in np.arange(1, self.Nrows-1):
-        #     for j in np.arange(self.Ncols):
-        #         if self.cells[i, j].body:
-
-        # print(self.body)
 
         for bodycell in self.body:
 
-            # print(bodycell)
-
             i = bodycell.loc[0]
             j = bodycell.loc[1]
-
-            # print('first ', len(self.candidates))
 
             # source and sinks can't become part of the body
             if i != 1:
@@ -194,45 +203,41 @@ class Grid(object):
         '''
 
         # concentration all candidates
-        # total = np.sum([self.cells[i, j].C ** self.eta for i in np.arange(self.Nrows) for j in np.arange(self.Ncols) if self.cells[i, j].candidate])
         total = np.sum([candidate.C**self.eta for candidate in self.candidates])
-
-        # for i in np.arange(self.Nrows):
-        #     for j in np.arange(self.Ncols):
-        #       if self.cells[i, j].candidate:
 
         for candidate in self.candidates:
             i = candidate.loc[0]
             j = candidate.loc[1]
 
             self.cells[i, j].P_growth = (self.cells[i, j].C ** self.eta) / total
-
-        # else:
-        #     self.cells[i, j].P_growth = 0 
                            
 
     def DLA(self):
         '''
         Diffusion Limted Aggregattion algorithm
         '''
-        time_grid = [[[self.cells[i, j].C for j in np.arange(self.Ncols)] for i in np.arange(self.Nrows)]]
 
         self.true_solution()
+
+        time_grid = [[[self.cells[i, j].C for j in np.arange(self.Ncols)] for i in np.arange(self.Nrows)]]
 
         for k in range(self.steps):
 
             # calculate the diffusion over the next time step
-            # print('yeet')
-            self.SOR()
-            # print('yoot')
+            if self.method == 'SOR':
+                self.SOR()
+            elif self.method == 'shifts':
+                self.shift_solve()
+            else:
+                raise ValueError(f'mehtod can be SOR or shifts not {self.method}')
         
+
             self.update_candidates()
 
             # determine growth probabilities for all candidates
             self.set_P_growth()
 
             # pick a candidate with the growth probability as weight
-            # p = np.array([self.cells[i, j].P_growth for i in range(self.Nrows) for j in range(self.Ncols)])
             p = np.array([candidate.P_growth for candidate in self.candidates])
 
             # new_object = np.random.choice(self.cells.ravel(), p=p)
@@ -242,10 +247,6 @@ class Grid(object):
             obj_coords = np.where(self.cells == new_object)
 
             # add choosen candidate to the body and remove from candidates
-            # added_cell = Cell(0, False, True, np.random.random())
-            # self.cells[obj_coords[0], obj_coords[1]] = added_cell
-            # self.body += [added_cell]
-            # print(self.cells[obj_coords[0], obj_coords[1]])
             self.cells[obj_coords[0], obj_coords[1]][0].in_body = True
             self.cells[obj_coords[0], obj_coords[1]][0].candidate = False
             self.cells[obj_coords[0], obj_coords[1]][0].C = 0
@@ -255,10 +256,9 @@ class Grid(object):
             self.body += [self.cells[obj_coords[0], obj_coords[1]][0]]
             self.candidates.remove(self.cells[obj_coords[0], obj_coords[1]][0])
 
-            # time_grid += [[[self.cells[i, j].C for j in np.arange(self.Ncols)] for i in np.arange(self.Nrows)]]
             for bodycell in self.body:
                 self.concentrations[bodycell.loc[0], bodycell.loc[1]] = 0
-            self.concentrations[self.Nrows-1,:] = 0
+            self.concentrations[-1,:] = 0
             time_grid += [self.concentrations]
 
             if k%100==0:
@@ -337,67 +337,39 @@ def plot_DLA():
 
 if __name__ == '__main__':
 
-    steps = 1000
+    steps = 100
     size = (100, 100)
     seed = (size[1]-1, int(size[1]/2))
     w = 1.85
     eta = 1.5
     epsilon = 1e-3
+    method = 'shifts'
 
-    # model = Grid(size, w, eta, seed, steps, epsilon)
-    # grid, iters = model.DLA()
-
-    # fig, ax = plt.subplots()
-    
-    # im = ax.imshow(grid[0])
-
-    # def animate(i):
-    #     i -= 1
-    #     im.set_array(grid[i])  # update the data
-    #     return [im]
-
-
-    # # Init only required for blitting to give a clean slate.
-    # def init():
-    #     im.set_data(grid[0])
-    #     return [im]
-
-    # ani = animation.FuncAnimation(fig, animate, init_func=init,
-    #                             interval=25, blit=True)
-
-
-    # # writergif = animation.PillowWriter(fps=60, bitrate=-1) 
-    # # ani.save('diffeq.gif', writer=writergif)
-    # plt.show()
-
-    # eta = 0.1
-    # model = Grid(size, w, eta, seed, steps, epsilon)
-    # grid, iters = model.DLA()
-
-    # df = pd.DataFrame(grid[-1])
-    # df.to_csv('DLA_0.1.csv')
-
-    # eta = 1
-    # model = Grid(size, w, eta, seed, steps, epsilon)
-    # grid, iters = model.DLA()
-
-    # df = pd.DataFrame(grid[-1])
-    # df.to_csv('DLA_1.csv')
-
-    eta = 2
-    model = Grid(size, w, eta, seed, steps, epsilon)
+    model = Grid(size, w, eta, seed, steps, epsilon, method)
     grid, iters = model.DLA()
 
-    df = pd.DataFrame(grid[-1])
-    df.to_csv('DLA_2.csv')
-
-    eta = 3
-    model = Grid(size, w, eta, seed, steps, epsilon)
-    grid, iters = model.DLA()
-
-    df = pd.DataFrame(grid[-1])
-    df.to_csv('DLA_3.csv')
+    fig, ax = plt.subplots()
     
+    im = ax.imshow(grid[0])
 
-    plot_DLA()
+    def animate(i):
+        i -= 1
+        im.set_array(grid[i])  # update the data
+        return [im]
+
+
+    # Init only required for blitting to give a clean slate.
+    def init():
+        im.set_data(grid[0])
+        return [im]
+
+    ani = animation.FuncAnimation(fig, animate, init_func=init,
+                                interval=25, blit=True)
+
+
+    # writergif = animation.PillowWriter(fps=60, bitrate=-1) 
+    # ani.save('diffeq.gif', writer=writergif)
+    plt.show()
+
+    # plot_DLA()
 
